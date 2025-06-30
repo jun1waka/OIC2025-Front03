c7335f5211f33d14c700ddc36c3ee0bf60b203bb
JUNICHI WAKABAYASHI <jun1@rimgate.net>
2025-06-30 10:33:07 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -36,6 +36,28 @@ function clearAll() {
     updateDisplay("0"); // ディスプレイも"0"に戻す
 }
 
+function calculate(n1, op, n2) {
+    let result = 0;
+    switch (op) {
+        case '+':
+            result = n1 + n2;
+            break;
+        case '-':
+            result = n1 - n2;
+            break;
+        case '*':
+            result = n1 * n2;
+            break;
+        case '/':
+            if (n2 === 0) return "Error";
+            result = n1 / n2;
+            break;
+        default:
+            return n2; 
+    }
+    return Math.round(result * 10000000000) / 10000000000;
+}
+
 // 最初に画面が表示されたとき、ディスプレイに初期値 "0" を表示する
 updateDisplay(currentInput);
 
@@ -45,17 +67,24 @@ updateDisplay(currentInput);
 switches.forEach(function (button) {
     button.addEventListener('click', function (e) {
         const value = e.target.textContent; // クリックされたボタンのテキストを取得
-        if (currentInput === "0" && value !== ".") {
-            // 入力が"0"だけの時に、"."以外の数字が押されたら、上書き
+        // 演算子が押された直後か？
+        if (isWaitingForSecondOperand === true) {
+            // 直後なら、新しい数値で入力を上書きし、フラグを戻す
             currentInput = value;
-        }else if (value === ".") {
-            // 小数点が押された場合
-            if (!isDot) { // まだ小数点が入力されていなければ
-                currentInput += value; // 追記し、
-                isDot = true;          // 小数点が入力されたことを記録する
-            }
-        }else{
-            currentInput += value; // 追記し、
+            isWaitingForSecondOperand = false;
+        } else {
+	        if (currentInput === "0" && value !== ".") {
+	            // 入力が"0"だけの時に、"."以外の数字が押されたら、上書き
+	            currentInput = value;
+	        }else if (value === ".") {
+	            // 小数点が押された場合
+	            if (!isDot) { // まだ小数点が入力されていなければ
+	                currentInput += value; // 追記し、
+	                isDot = true;          // 小数点が入力されたことを記録する
+	            }
+	        }else{
+	            currentInput += value; // 追記し、
+	        }
         }
         updateDisplay(currentInput); // 画面を更新
         console.log('数字ボタン:', e.target.textContent);
@@ -64,6 +93,16 @@ switches.forEach(function (button) {
 // 演算子ボタンの処理
 calculations.forEach(function (button) {
     button.addEventListener('click', function (e) {
+        const newOperator = e.target.textContent;
+        if (isWaitingForSecondOperand) {
+            operator = newOperator;
+            return;
+        }
+        if (left === null) {
+            left = parseFloat(currentInput);
+        }
+        operator = newOperator;
+        isWaitingForSecondOperand = true;
         console.log('演算子ボタン:', e.target.textContent);
     });
 });
@@ -79,6 +118,14 @@ resets.forEach(function (button) {
 // =ボタンの処理
 results.forEach(function (button) {
     button.addEventListener('click', function () {
+        if (left === null || operator === "" || isWaitingForSecondOperand){
+            return;
+        }
+        const right = parseFloat(currentInput);
+        const result = calculate(left, operator, right); 
+
+        updateDisplay(result);
+
         console.log('=ボタンが押されました');
     });
 });
